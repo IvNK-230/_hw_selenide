440efb88e6a8b44715c86a4e54a8aa6b416a2253
Ivan Kashuba <kashubaim@gmail.com>
2023-09-27 22:49:36 +0600

    change test

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -1,29 +1,34 @@
 package ru.netology;
 
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
-    String currentDate = String.valueOf(LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd-MM-yyyy")));
+    public String generateDate(long addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    String planningDate = generateDate(3, "dd.MM.yyyy");
 
     @Test
     void ShouldDoneTest() {
         open("http://localhost:9999/");
         $("[data-test-id=city] input").setValue("Белгород");
-        $("[data-test-id=date] input").doubleClick().sendKeys(currentDate);
+        $("[data-test-id=date] input").doubleClick().sendKeys(planningDate);
         $("[data-test-id=name] input").setValue("Петров Максим");
         $("[name='phone']").setValue("+79310016666");
         $("[data-test-id=agreement]").click();
         $(byText("Забронировать")).click();
-        $(withText("Встреча успешно забронирована")).shouldBe(visible, Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
     }
 
 }
